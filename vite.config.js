57a52ef6a9b4cc614aa9619a929ff6891469044e
Pedro Blue <pedroblue@outlook.com.br>
2023-10-20 03:01:22 -0300

    Modelo de Ecommerce, focado no CRUDE e experience de finalizar compras

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/E-comerce/",
+  base: "/Ecomerce/",
   plugins: [],
   resolve: {
     /*something*/
